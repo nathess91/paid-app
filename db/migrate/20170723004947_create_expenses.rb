8faca5b9375e1f8ce3d2c7cae9f8bae60426352f
Natalia Hess <nathess91@gmail.com>
2017-07-22 17:51:12 -0700

    Adds Expense model and controller

---
+++ db/migrate/20170723004947_create_expenses.rb
@@ -0,0 +1,12 @@
+class CreateExpenses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :expenses do |t|
+      t.string :category
+      t.decimal :amount
+      t.integer :frequency
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170723002749) do
+ActiveRecord::Schema.define(version: 20170723004947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "expenses", force: :cascade do |t|
+    t.string "category"
+    t.decimal "amount"
+    t.integer "frequency"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_expenses_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,4 +47,5 @@ ActiveRecord::Schema.define(version: 20170723002749) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "expenses", "users"
 end
