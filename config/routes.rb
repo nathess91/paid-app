d9800678647135142030ba61ad86be753ebe3c15
Natalia Hess <nathess91@gmail.com>
2017-07-22 18:39:35 -0700

    Sets up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,28 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_scope :user do
+       get "signup", to: "devise/registrations#new"
+       get "login", to: "devise/sessions#new"
+       get "logout", to: "devise/sessions#destroy"
+
+    authenticated :user do
+      root 'bills#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  resources :bills do
+    resources :payments
+  end
+
+  resources :expenses
+
 end
