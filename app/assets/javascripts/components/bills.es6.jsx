c8e07916a5a1f35ed658e94ccb4948b785c3e166
Natalia Hess <nathess91@gmail.com>
2017-07-24 17:48:34 -0700

    Creates initial cards for individual bills

--- app/assets/javascripts/components/bills.es6.jsx
+++ app/assets/javascripts/components/bills.es6.jsx
@@ -2,7 +2,18 @@ class Bills extends React.Component {
   render () {
     const billData = this.props.bills.map((bill, index) => {
       return (
-        <p key={index}>{bill.name}</p>
+        <div className="col s12 m7">
+        <div className="card horizontal">
+          <div className="card-content">
+            <p>{bill.due_date}</p>
+          </div>
+          <div className="card-stacked">
+            <div className="card-content">
+              <p>{bill.name.toUpperCase()}</p>
+            </div>
+          </div>
+        </div>
+        </div>
       );
     });
     return (
